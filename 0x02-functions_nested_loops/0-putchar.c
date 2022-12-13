bd2f7cdf832c4583e0c7eed8bc8b44c4283a9fea
Liboire HADJA <liboirehadja@gmail.com>
2022-12-13 11:57:16 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Write a program that prints _putchar,
+ * followed by a new line.
+ *
+ * Return Always 0 (success)
+ */
+int main(void)
+{
+	char ch = 's';
+
+	_putchar(ch);
+	_putchar('\n');
+
+	return (0);
+}
